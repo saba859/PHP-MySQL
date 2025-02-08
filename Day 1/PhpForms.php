e82f283162f8b0104f4430f8109359462b3003d8
saba859 <sashrafi869@gmail.com>
2025-02-08 23:18:02 +0530

    Delete Day 1/PhpForms.php

--- Day 1/PhpForms.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "Current Script:". $_SERVER['PHP_SELF']. "<br>";
-echo "Server name:". $_SERVER['SERVER_NAME']."<br>";
-echo "Client IP:". $_SERVER['REMOTE_ADDR']. "<br>";
-echo "Request method:". $_SERVER['REQUEST_METHOD']. "<br>";
-?>
